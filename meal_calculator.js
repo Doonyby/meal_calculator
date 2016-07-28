b3966190915c470fadf83b77d8e9729a02b9d718
Dan Riding <doonyby@gmail.com>
2016-07-28 10:11:09 -0600

    finalized tab distribution

--- meal_calculator.js
+++ meal_calculator.js
@@ -30,30 +30,28 @@ Party.prototype.addTax = function() {
 
 Party.prototype.addTip = function() {
 	this.withTip += this.withTax + (0.2 * this.withTax);
-	this.withTip = Math.round(this.withTip, 2);
-	console.log(this.withTip);
+	this.withTip = this.withTip.toFixed(2);
+	this.withTip = parseFloat(this.withTip);
 }
 
 Party.prototype.divideTab = function() {
 	this.dinerPay += this.withTip/this.diners.length;
+	this.dinerPay = this.dinerPay.toFixed(2);
+	this.dinerPay = parseFloat(this.dinerPay);
 	if (this.withTip % this.dinerPay == 0) {
 		for (var i=0; i<this.diners.length; i++) {
-			console.log(this.diners[i].name + " owes " + this.dinerPay.toFixed(2) + "!");
+			console.log(this.diners[i].name + " owes " + this.dinerPay + "!");
 		}
 	} else {
 		var lastDiner = this.diners.pop();
-		var remainderAmount = (this.withTip % this.dinerPay);
-		console.log(remainderAmount + this.dinerPay);
+		var remainder = (this.withTip % this.dinerPay);
+		var remainingAmount = remainder + this.dinerPay;
 		for (var i=0; i<this.diners.length; i++) {
-			console.log(this.diners[i].name + " owes " + this.dinerPay.toFixed(2) + "!");
+			console.log(this.diners[i].name + " owes " + this.dinerPay + "!");
 		}
-		console.log(lastDiner.name + " owes " + remainderAmount.toFixed(2) + "!");
+		console.log(lastDiner.name + " owes " + remainingAmount.toFixed(2) + "!");
 	}
 }
-
-var partyOfFive = new Party();
-
-
 
 var Diner = function(name) {
 	this.name = name;
@@ -76,46 +74,49 @@ Diner.prototype.dinerTotal = function() {
 	}
 }
 
-var bob = new Diner('bob');
-bob.addDish('king crab');
-bob.addDish('drink');
-bob.dinerTotal();
+function mealCalculator() {
+	var bob = new Diner('bob');
+	bob.addDish('king crab');
+	bob.addDish('drink');
+	bob.dinerTotal();
 
-var john = new Diner('john');
-john.addDish('filet mignon');
-john.addDish('salad');
-john.addDish('soup');
-john.addDish('dessert');
-john.dinerTotal();
+	var john = new Diner('john');
+	john.addDish('filet mignon');
+	john.addDish('salad');
+	john.addDish('soup');
+	john.addDish('dessert');
+	john.dinerTotal();
 
-var jane = new Diner('jane');
-jane.addDish('roasted chicken');
-jane.addDish('soup');
-jane.addDish('drink');
-jane.addDish('dessert');
-jane.dinerTotal();
+	var jane = new Diner('jane');
+	jane.addDish('roasted chicken');
+	jane.addDish('soup');
+	jane.addDish('drink');
+	jane.addDish('dessert');
+	jane.dinerTotal();
 
-var lynn = new Diner('lynn');
-lynn.addDish('roasted duck');
-lynn.addDish('drink');
-lynn.dinerTotal();
+	var lynn = new Diner('lynn');
+	lynn.addDish('roasted duck');
+	lynn.addDish('drink');
+	lynn.dinerTotal();
 
-var joe = new Diner('joe');
-joe.addDish('roasted vegetables');
-joe.addDish('dessert');
-joe.addDish('drink');
-joe.dinerTotal();
+	var joe = new Diner('joe');
+	joe.addDish('roasted vegetables');
+	joe.addDish('dessert');
+	joe.addDish('drink');
+	joe.dinerTotal();
 
-partyOfFive.diners.push(bob);
-partyOfFive.diners.push(john);
-partyOfFive.diners.push(jane);
-partyOfFive.diners.push(lynn);
-partyOfFive.diners.push(joe);
+	var partyOfFive = new Party();
 
+	partyOfFive.diners.push(bob);
+	partyOfFive.diners.push(john);
+	partyOfFive.diners.push(jane);
+	partyOfFive.diners.push(lynn);
+	partyOfFive.diners.push(joe);
 
-partyOfFive.addTotal();
-partyOfFive.addTax();
-partyOfFive.addTip();
-partyOfFive.divideTab();
-console.log(partyOfFive);
+	partyOfFive.addTotal();
+	partyOfFive.addTax();
+	partyOfFive.addTip();
+	partyOfFive.divideTab();
+}
 
+mealCalculator();
